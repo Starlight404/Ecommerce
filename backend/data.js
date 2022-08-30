f69daaea654643d95ef73ab32c2e37a86bc5100c
Starlight404 <rahulkpcaiub@gmail.com>
2022-08-30 16:15:33 +0600

    wip: product page is getting ready

--- backend/data.js
+++ backend/data.js
@@ -18,7 +18,7 @@ const data = {
       category: "Shirts",
       image: "/images/slim2.jpg",
       price: 225,
-      countInStock: 10,
+      countInStock: 0,
       brand: "Addidas",
       rating: 4.5,
       numReviews: 10,
