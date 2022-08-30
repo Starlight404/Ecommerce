28bf5eaca3608e39e7351934557f776906a7ad8c
Starlight404 <rahulkpcaiub@gmail.com>
2022-08-31 00:01:23 +0600

    wip: fixed add to cart

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: "1",
       name: "Nike slim shirts",
       slug: "nike-slim-shirts",
       category: "Shirts",
@@ -13,6 +14,7 @@ const data = {
       description: "premium quality",
     },
     {
+      _id: "2",
       name: "Addidas slim fit",
       slug: "addidas-slim-fit",
       category: "Shirts",
@@ -25,6 +27,7 @@ const data = {
       description: "premium quality cotton",
     },
     {
+      _id: "3",
       name: "Addias slim jersy",
       slug: "addias-slim-jersy",
       category: "Jersy",
@@ -37,6 +40,7 @@ const data = {
       description: "premium quality cotton",
     },
     {
+      _id: "4",
       name: "Barca New Home Kit",
       slug: "barca-jersy-new",
       category: "Jersy",
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,18 @@ app.get("/api/products", (req, res) => {
 });
 app.get("/api/products/slug/:slug", (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
-  if(product) {
+  if (product) {
     res.send(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
   }
-  else {
-    res.status(404).send({message:"product not found"})
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
   }
 });
 
