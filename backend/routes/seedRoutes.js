d911e1cabd0174bcbe5c16e823a2ec50ec8435fe
Starlight404 <rahulkpcaiub@gmail.com>
2022-09-03 00:21:59 +0600

    wip: done fetching sample data from database

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -4,9 +4,9 @@ import data from "../data.js";
 const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
-  await Product.remove({}); 
-  const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts }); 
+  await Product.remove({}); //returns all records inside product model
+  const createdProducts = await Product.insertMany(data.products); //we call this to insert an array of products to the product model in the database
+  res.send({ createdProducts }); // send this to the frontend
 });
 
 export default seedRouter;
