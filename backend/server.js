058ca41855ef656420506d429c0e957d64bf0b93
Starlight404 <rahulkpcaiub@gmail.com>
2022-09-04 23:19:57 +0600

    wip:done creating backend api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config(); //for fetching data
 
@@ -17,14 +18,17 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));//the form data in the post request will be converted to a json object inside req.body
 
 app.use("/api/seed", seedRouter);
 
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
 //its a middleware that accpets four parameters
-app.use((err, req,res,next) => {
-  res.status(500).send({message: err.message});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
